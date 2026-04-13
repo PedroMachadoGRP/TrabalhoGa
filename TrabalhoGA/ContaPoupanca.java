5a2b62a7113c6da5b9efe5fd5fccb7f5255dee01
Otto Machado <158228292+OttoMachado@users.noreply.github.com>
2026-04-12 22:39:50 -0300

    Update ContaPoupanca.java

--- TrabalhoGA/ContaPoupanca.java
+++ TrabalhoGA/ContaPoupanca.java
@@ -4,19 +4,39 @@ public class ContaPoupanca extends ContaBancaria {
     // Dia do mes em que a poupanca rende juros
     private int diaAniversario;
 
-    public void movimenta(Operacao op) {
+    public ContaPoupanca(Cliente cliente) {
+    this.setCliente(cliente);
+}
+
+    public boolean movimenta(Operacao op) {
         // Deposito: soma o valor ao saldo
         if (op.getTipo() == 'D') {
             setSaldoAtual(getSaldoAtual() + op.getValor());
             getDeposito().registrar(op.getValor());
+            return true;
 
         // Saque: verifica se tem saldo suficiente
         } else if (op.getTipo() == 'S') {
+            if (op.getValor() <= 0) {
+                System.out.println("O valor de saque deve ser maior do que zero.");
+                return false;
+            }
+
             if (op.getValor() > getSaldoAtual()) {
-                System.out.println("Saldo insuficiente!");
-            } else {
+                System.out.println("O saldo e insuficiente!");
+                return false;
+            }
+
+            int[] cedulas = calcularCedulas(op.getValor());
+
+            if (cedulas == null) {
+                System.out.println("Nao foi possivel gerar esse valor com as cedulas disponiveis.");
+                return false;
+            }else {
                 setSaldoAtual(getSaldoAtual() - op.getValor());
                 getSaques().registrar(op.getValor());
+                imprimirCedulas(cedulas);
+                return true;
             }
 
         // Juros: calcula e aplica a taxa percentual sobre o saldo
@@ -24,7 +44,9 @@ public class ContaPoupanca extends ContaBancaria {
             double valorJuros = getSaldoAtual() * (op.getValor() / 100.0);
             setSaldoAtual(getSaldoAtual() + valorJuros);
             getJuros().registrar(valorJuros);
+            return true;
         }
+        return false;
     }
 
     // Getters e Setters
