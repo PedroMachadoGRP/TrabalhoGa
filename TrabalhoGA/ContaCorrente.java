7aad66c6379db981241d0e16d511d84e310e5665
Otto Machado <158228292+OttoMachado@users.noreply.github.com>
2026-04-12 22:38:21 -0300

    Adicionando comentarios

--- TrabalhoGA/ContaCorrente.java
+++ TrabalhoGA/ContaCorrente.java
@@ -4,22 +4,43 @@ public class ContaCorrente extends ContaBancaria {
     // Limite extra alem do saldo que o cliente pode usar
     private double limiteCredito;
 
-    @Override
-    public void movimenta(Operacao op) {
+    public ContaCorrente(int diaAniversario, Cliente cliente) {
+        this.setCliente(cliente);
+    }
+
+    public boolean movimenta(Operacao op) {
         // Deposito: soma o valor ao saldo
         if (op.getTipo() == 'D') {
             setSaldoAtual(getSaldoAtual() + op.getValor());
             getDeposito().registrar(op.getValor());
+            return true;
 
         // Saque: verifica se tem saldo + limite disponivel
         } else if (op.getTipo() == 'S') {
-            if (op.getValor() > getSaldoAtual() + limiteCredito) {
-                System.out.println("Saldo insuficiente!");
-            } else {
-                setSaldoAtual(getSaldoAtual() - op.getValor());
-                getSaques().registrar(op.getValor());
+            if (op.getValor() <= 0) {
+            System.out.println("O valor de saque deve ser maior do que zero.");
+            return false;
             }
-        }
+            
+            if (op.getValor() > getSaldoAtual() + limiteCredito) {
+            System.out.println("O saldo é insuficiente!");
+            return false;
+            }
+
+            int[] cedulas = calcularCedulas(op.getValor());
+
+            if (cedulas == null) {
+            System.out.println("Nao foi possivel gerar esse valor com as cédulas disponiveis.");
+            return false;
+            }
+            
+            setSaldoAtual(getSaldoAtual() - op.getValor());
+            getSaques().registrar(op.getValor());
+            imprimirCedulas(cedulas);
+            return true;
+            }  
+            
+        return false;
     }
 
     // Getters e Setters
