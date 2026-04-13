440e77ee1be075846370afc0a3fe101db25026e9
Otto Machado <158228292+OttoMachado@users.noreply.github.com>
2026-04-12 22:38:59 -0300

    Adicionando comentarios

--- TrabalhoGA/ContaInvestimento.java
+++ TrabalhoGA/ContaInvestimento.java
@@ -4,18 +4,26 @@ public class ContaInvestimento extends ContaBancaria {
     // Data em que o investimento vence
     private Data dtVencimento;
 
-    public void movimenta(Operacao op) {
+    public ContaInvestimento(Data dtVencimento, Cliente cliente) {
+    this.dtVencimento = dtVencimento;
+    this.setCliente(cliente);
+}
+
+    public boolean movimenta(Operacao op) {
         // Deposito: soma o valor ao saldo
         if (op.getTipo() == 'D') {
             setSaldoAtual(getSaldoAtual() + op.getValor());
             getDeposito().registrar(op.getValor());
+            return true;
 
         // Juros: calcula e aplica a taxa percentual sobre o saldo
         } else if (op.getTipo() == 'J') {
             double valorJuros = getSaldoAtual() * (op.getValor() / 100.0);
             setSaldoAtual(getSaldoAtual() + valorJuros);
             getJuros().registrar(valorJuros);
+            return true;
         }
+        return false;
     }
 
     // Getters e Setters
